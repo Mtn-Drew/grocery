ba726dede66e6c23c66bb4f9c9be4e2d09d3b600
Drew Carrillo <drewc3@gmail.com>
2020-05-23 12:39:34 -0600

    build folder

---
+++ client/build/precache-manifest.7bbf0b4db3a012804bcb5f7e60355b3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da04683fb8caf8d209435bc4b76f5fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19845eed5b2fc0f169bc",
+    "url": "/static/css/2.dd1866a9.chunk.css"
+  },
+  {
+    "revision": "1be49bcf98444f7e0f71",
+    "url": "/static/css/main.f549005f.chunk.css"
+  },
+  {
+    "revision": "19845eed5b2fc0f169bc",
+    "url": "/static/js/2.cfe29cb5.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.cfe29cb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be49bcf98444f7e0f71",
+    "url": "/static/js/main.fb80dea0.chunk.js"
+  },
+  {
+    "revision": "aec1a5a07d23a09a57ec",
+    "url": "/static/js/runtime-main.45a608ac.js"
+  }
+]);
