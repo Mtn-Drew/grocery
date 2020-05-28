7aaa04e340cf3176afd8fcd8e707197aaadb5f19
Drew Carrillo <drewc3@gmail.com>
2020-05-28 04:52:18 -0600

    add modal

--- client/src/context/AppReducer.js
+++ client/src/context/AppReducer.js
@@ -38,66 +38,66 @@ export default (state, action) => {
       }
 
     case 'TOGGLE_MODAL':
-      const modal = (
-        <div className={`modalBackground modalShowing-${!state.showModal}`}>
-          {console.log('showMOdal is --', state.showModal)}
-          <form onSubmit={() => {}} className="modalInner">
-            <div className="form-control">
-              <label htmlFor="text">Grocery Item</label>
-              <input
-                type="text"
-                placeholder="grocery item name"
-                value={action.payload.itemName}
-                // onChange={(e) => setNewItemName(e.target.value)}
-                // required
-                readOnly
-              />
-            </div>
+      // const modal = (
+      //   <div className={`modalBackground modalShowing-${!state.showModal}`}>
+      //     {console.log('showMOdal is --', state.showModal)}
+      //     <form onSubmit={() => {}} className="modalInner">
+      //       <div className="form-control">
+      //         <label htmlFor="text">Grocery Item</label>
+      //         <input
+      //           type="text"
+      //           placeholder="grocery item name"
+      //           value={action.payload.itemName}
+      //           // onChange={(e) => setNewItemName(e.target.value)}
+      //           // required
+      //           readOnly
+      //         />
+      //       </div>
 
-            <div className="form-control">
-              <input
-                type="text"
-                placeholder="description"
-                // value={newItemDescription}
-                // onChange={(e) => setNewItemDescription(e.target.value)}
-              />
-            </div>
-            <div className="form-control">
-              <input
-                type="text"
-                placeholder="aisle"
-                value={action.payload.aisle}
-                // onChange={(e) => setNewItemAisle(e.target.value)}
-                readOnly
-              />
-            </div>
-            <div className="form-control">
-              <input
-                type="text"
-                placeholder="store"
-                value={action.payload.defaultStore}
-                // onChange={(e) => setNewItemStore(e.target.value)}
-                readOnly
-              />
-            </div>
-            <div className="form-control">
-              <input
-                type="text"
-                placeholder="date last purchased"
-                // value={newItemLastPurchased}
-                readOnly
-              />
-            </div>
+      //       <div className="form-control">
+      //         <input
+      //           type="text"
+      //           placeholder="description"
+      //           // value={newItemDescription}
+      //           // onChange={(e) => setNewItemDescription(e.target.value)}
+      //         />
+      //       </div>
+      //       <div className="form-control">
+      //         <input
+      //           type="text"
+      //           placeholder="aisle"
+      //           value={action.payload.aisle}
+      //           // onChange={(e) => setNewItemAisle(e.target.value)}
+      //           readOnly
+      //         />
+      //       </div>
+      //       <div className="form-control">
+      //         <input
+      //           type="text"
+      //           placeholder="store"
+      //           value={action.payload.defaultStore}
+      //           // onChange={(e) => setNewItemStore(e.target.value)}
+      //           readOnly
+      //         />
+      //       </div>
+      //       <div className="form-control">
+      //         <input
+      //           type="text"
+      //           placeholder="date last purchased"
+      //           // value={newItemLastPurchased}
+      //           readOnly
+      //         />
+      //       </div>
 
-            <button className="btn">Update</button>
-          </form>
-        </div>
-      )
+      //       <button className="btn">Update</button>
+      //     </form>
+      //   </div>
+      // )
       const flip = !state.showModal
       return {
         ...state,
         showModal: flip,
-        modal: modal,
+        // modal: modal,
       }
 
     case 'GET_GROCERIES':
